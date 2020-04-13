27d9558207af4b0cb90adaf5a1f36323d5543c48
patel1678 <patel_1678@yahoo.com>
2020-04-13 15:03:41 +0530

    HellowZS.java created

---
+++ HelloZS.java
@@ -0,0 +1,11 @@
+
+class HelloZS
+{
+	public static void main(String args[]) {
+	
+	System.out.println("Hello, Zendriix Softwares"); 
+	System.out.println("This is frist commit of this solution");
+
+	}
+
+}
